750563fa2421a709515624bba1cb521301b85283
tleef <the.tleef@gmail.com>
2016-06-30 13:48:41 -0700

    Camel case all function names

--- s3Functions.js
+++ s3Functions.js
@@ -1,8 +1,9 @@
 var AWS = require('aws-sdk');
+var camelCase = require('camel-case');
 
 module.exports = Object.keys(
   AWS.apiLoader(
     AWS.S3.serviceIdentifier,
     '2006-03-01'
   ).operations
-);
+).map(camelCase);
