6150fb5366aa7740d18a701b5142ef8be8a11cd3
Daniel Yakobian <daniel.yakobian@gotinder.com>
2016-07-06 11:07:38 -0700

    Several functions were missing when using previous method for extracting function names, useing hard coded strings now

--- s3Functions.js
+++ s3Functions.js
@@ -1,9 +1,13 @@
-var AWS = require('aws-sdk');
-var camelCase = require('camel-case');
+var func_names=['abortMultipartUpload', 'completeMultipartUpload', 'copyObject', 'createBucket', 'createMultipartUpload',
+  'deleteBucket', 'deleteBucketCors', 'deleteBucketLifecycle', 'deleteBucketPolicy', 'deleteBucketReplication', 'deleteBucketTagging',
+  'deleteBucketWebsite', 'deleteObject', 'deleteObjects', 'getBucketAccelerateConfiguration', 'getBucketAcl', 'getBucketCors',
+  'getBucketLifecycle', 'getBucketLifecycleConfiguration', 'getBucketLocation', 'getBucketLogging', 'getBucketNotification',
+  'getBucketNotificationConfiguration', 'getBucketPolicy', 'getBucketReplication', 'getBucketRequestPayment', 'getBucketTagging',
+  'getBucketVersioning', 'getBucketWebsite', 'getObject', 'getObjectAcl', 'getObjectTorrent', 'getSignedUrl', 'headBucket', 'headObject', 'isValidAccelerateOperation', 'listBuckets',
+  'listMultipartUploads', 'listObjectVersions', 'listObjects', 'listObjectsV2', 'listParts', 'putBucketAccelerateConfiguration', 'putBucketAcl',
+  'putBucketCors', 'putBucketLifecycle', 'putBucketLifecycleConfiguration', 'putBucketLogging', 'putBucketNotification',
+  'putBucketNotificationConfiguration', 'putBucketPolicy', 'putBucketReplication', 'putBucketRequestPayment',
+  'putBucketTagging', 'putBucketVersioning', 'putBucketWebsite', 'putObject', 'putObjectAcl', 'restoreObject', 'upload',
+  'uploadPart', 'uploadPartCopy', 'waitFor'];
 
-module.exports = Object.keys(
-  AWS.apiLoader(
-    AWS.S3.serviceIdentifier,
-    '2006-03-01'
-  ).operations
-).map(camelCase);
+module.exports = func_names;
